c46692eedbd45176f0c89da8ca8b54f4335f062b
Grace <cgracesharpe@gmail.com>
2022-02-24 15:29:27 -0500

    add note route

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-const apiRoutes = require('../routes/apiRoutes');
-const htmlRoutes = require('../routes/htmlRoutes');
+const apiRoutes = require('../../routes/apiRoutes');
+const htmlRoutes = require('../../routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +12,11 @@ app.use(express.static('public'));
 // Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+const { notes } = require('./develop/db');
+app.get('/api/notes', (req, res) => {
+      res.json(notes);
+  });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
