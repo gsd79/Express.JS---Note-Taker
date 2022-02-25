e87dfd500bae8173615c05bcd15b94371e866823
Grace <cgracesharpe@gmail.com>
2022-02-24 20:19:24 -0500

    got everything functional bar delete

--- server.js
+++ server.js
@@ -1,35 +1,61 @@
-const express = require('express');
-const app = express();
 const fs = require('fs');
 const path = require('path');
 
+const express = require('express');
+const { notes } = require('./data/notes');
+
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('develop/public'));
+app.use(express.static('public'));
 
 
-const { notes } = require('./develop/db/db.json');
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+  }
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+     res.json(notes);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+      res.json(result);
+  });
+
 app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createLi(req.body, notes);
+    req.body.id = notes.length.toString();  
+    const note = createNewNote (req.body, notes);
     res.json(note);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/index.html'));
-});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+      );
+      return note;
+   
+}
+
+
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
-});
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+  
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 
 
 app.listen(PORT, () => {
