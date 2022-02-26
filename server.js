596508c18e89ed36fd1f4123d6a8fd07a29c8609
Grace <cgracesharpe@gmail.com>
2022-02-26 15:47:48 -0500

    delete method functionality

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 // dependancy
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const {notes} = require('./db/db.json');
 
 // variables
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const path = require('path');
-const fs = require('fs');
-const { notes } = require('./db/db');
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -15,36 +14,50 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+//api routes
+app.get('/api/notes', (req,res) => {
+      fs.readFile('./db/db.json', 'utf8' , (err, notes) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(notes)
+      })
+    res.json(notes)
+})
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req,res) => {
     req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
+    const note = req.body;
+    notes.push(note);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes }, null, 2))
+
     res.json(note);
-});
+})
 
+app.delete('/api/notes/:id', (req,res) => {
+    const { id } = req.params
+    const deletedNote = notes.find(notes => notes.id === id)
 
-function createNewNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db'),
-        JSON.stringify({ notes: notesArray }, null, 2)
-    );
-    return note;
+    if(deletedNote !== id ) {
+        notes.splice(id);
+        
+        console.log('Note #' +id + '  has been deleted')
+        fs.writeFileSync(path.join(__dirname, './db/db.json'),
+            JSON.stringify({ notes }, null, 2))
+    }
+})
 
-}
-
+// html routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes'));
-});             
-
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+  
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index'));
-});
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 
 
 // listen for port call 
