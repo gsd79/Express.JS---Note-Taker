e47c74f1befdd8eb16110f1b0c81f0703d33971f
Grace <cgracesharpe@gmail.com>
2022-02-26 00:33:56 -0500

    troubleshooting

--- public/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const noteRoutes = require('../routes/apiRoutes');
-const index = require('../routes/htmlRoutes');
-
-router.use(noteRoutes);
-router.use(index);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -4,18 +4,47 @@ const express = require('express');
 // variables
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./public/routes/apiRoutes');
-const htmlRoutes = require('./public/routes/htmlRoutes');
 
+const path = require('path');
+const fs = require('fs');
+const { notes } = require('./db/db');
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use routes
-app.use('/api/notes', apiRoutes);
-app.use('/', htmlRoutes);
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+
+}
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes'));
+});             
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index'));
+});
 
 
 // listen for port call 
